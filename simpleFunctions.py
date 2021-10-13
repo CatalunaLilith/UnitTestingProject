9d72c4b3e6e5b33fc577fbfba483bbf4ff6b1984
Cataluna <catalunalilith2680@gmail.com>
2021-10-13 11:25:23 -0400

    message

---
+++ simpleFunctions.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 11:21:51 2021
+
+@author: catal
+"""
+
+
+def add(x, y):
+    """assumes x and y are numbers
+    returns a number, representing x + y"""
+    return x + y
+
+
+def subtract(x, y):
+    """assumes x and y are numbers
+    returns a number, representing x - y"""
+    return x - y
+
+
+def multiply(x, y):
+    """assumes x and y are numbers
+    returns a number, representing x * y"""
+    return x * y
+
+
+def divide(x, y):
+    """assumes x and y are numbers
+    returns a number, representing x / y"""
+    return x / y
+
+
+def exponentiate(x, y):
+    """assumes x and y are numbers
+    returns a number, representing x ** y"""
+    return x ** y
