ec139db13ea85068d98401e2ef66d5922129b15e
Cataluna <catalunalilith2680@gmail.com>
2021-10-13 14:35:20 -0400

    first add test functional

--- test_simpleFunctions.py
+++ test_simpleFunctions.py
@@ -5,3 +5,14 @@ Created on Wed Oct 13 12:32:38 2021
 @author: catal
 """
 
+import unittest
+import simpleFunctions
+
+# print(simpleFunctions.add(1, 2))
+
+
+class TestSimpleFunctions(unittest.TestCase):
+
+    def test_add(self):
+        result = simpleFunctions.add(10, 5)
+        self.assertEqual(result, 15)
