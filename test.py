838cda475bb424f5e0a6aef90f151dc86b16fc3a
Cataluna <catalunalilith2680@gmail.com>
2021-10-15 19:08:27 -0400

    old unittest scripts updated to follow PEP8

--- test.py
+++ test.py
@@ -6,18 +6,20 @@ Created on Thu Dec  3 15:08:59 2020
 """
 
 import unittest
-
 from my_sum import sum_stuff
 
 
 class TestSum(unittest.TestCase):
+
     def test_list_int(self):
         """
         Test that it can sum a list of integers
         """
+        print("running test_list_int")
         data = [1, 2, 3]
         result = sum_stuff(data)
         self.assertEqual(result, 6)
+
 
 if __name__ == '__main__':
     unittest.main()
--- test_cuboid_volume.py
+++ test_cuboid_volume.py
@@ -7,12 +7,14 @@ Created on Fri May 28 15:45:52 2021
 import unittest
 from cuboid_volume import cuboid_volume
 
+
 class TestCuboid(unittest.TestCase):
     def test_volume(self):
-        self.assertAlmostEqual(cuboid_volume(2),8)
-        self.assertAlmostEqual(cuboid_volume(1),1)
-        self.assertAlmostEqual(cuboid_volume(0),0)
-        self.assertAlmostEqual(cuboid_volume(5.5),166.375)
-        
+        self.assertAlmostEqual(cuboid_volume(2), 8)
+        self.assertAlmostEqual(cuboid_volume(1), 1)
+        self.assertAlmostEqual(cuboid_volume(0), 0)
+        self.assertAlmostEqual(cuboid_volume(5.5), 166.375)
+
+
 if __name__ == '__main__':
     unittest.main()
