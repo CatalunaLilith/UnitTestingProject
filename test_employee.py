c3c884df9f39d255322e761c3801ae233716d6c2
Cataluna <catalunalilith2680@gmail.com>
2021-10-15 16:05:35 -0400

    basic tests for email, first, last, pay, raise(), and fullname() implemented. TODO: basic tests for monthly_schedule(), setup/teardown methods for employee instances for tests

--- test_employee.py
+++ test_employee.py
@@ -5,3 +5,49 @@ Created on Fri Oct 15 14:50:28 2021
 @author: catal
 """
 
+import unittest
+import employee
+
+
+class TestEmployee(unittest.TestCase):
+
+    def test_email(self):
+        emp_1 = employee.Employee("Hachi", "Goodyear", 80000)
+        emp_2 = employee.Employee("Midas", "Goodyear", 90000)
+        self.assertEqual(emp_1.email, "Hachi.Goodyear@email.com")
+        self.assertEqual(emp_2.email, "Midas.Goodyear@email.com")
+
+        emp_1.first = "Not-Muffin"
+        emp_2.last = "Daoust"
+        self.assertEqual(emp_1.email, "Not-Muffin.Goodyear@email.com")
+        self.assertEqual(emp_2.email, "Midas.Daoust@email.com")
+
+    def test_fullname(self):
+        emp_1 = employee.Employee("Hachi", "Goodyear", 80000)
+        emp_2 = employee.Employee("Midas", "Goodyear", 90000)
+        self.assertEqual(emp_1.fullname, "Hachi Goodyear")
+        self.assertEqual(emp_2.fullname, "Midas Goodyear")
+
+        emp_1.first = "Not-Muffin"
+        emp_2.last = "Daoust"
+        self.assertEqual(emp_1.fullname, "Not-Muffin Goodyear")
+        self.assertEqual(emp_2.fullname, "Midas Daoust")
+
+    def test_apply_raise(self):
+        emp_1 = employee.Employee("Hachi", "Goodyear", 80000)
+        emp_2 = employee.Employee("Midas", "Goodyear", 90000)
+
+        emp_1.apply_raise()
+        emp_2.apply_raise()
+        self.assertEqual(emp_1.pay, 84000)
+        self.assertEqual(emp_2.pay, 94500)
+
+        emp_1.apply_raise()
+        self.assertEqual(emp_1.pay, 88200)
+
+    # def test_monthly_schedule(self):
+    #     pass
+
+
+if __name__ == "__main__":
+    unittest.main()
