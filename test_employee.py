9212383fab1a60d7da30de76286d85f318edaae3
Cataluna <catalunalilith2680@gmail.com>
2021-10-15 17:23:12 -0400

    created and implemented setUp() method for creating employee objects for tests

--- test_employee.py
+++ test_employee.py
@@ -13,7 +13,8 @@ class TestEmployee(unittest.TestCase):
 
     def setUp(self):
         """is run before each test"""
-        pass
+        self.emp_1 = employee.Employee("Hachi", "Goodyear", 80000)
+        self.emp_2 = employee.Employee("Midas", "Goodyear", 90000)
 
     def tearDown(self):
         """is run after each test"""
@@ -21,40 +22,33 @@ class TestEmployee(unittest.TestCase):
 
     def test_email(self):
         """tests employee.email()"""
-        emp_1 = employee.Employee("Hachi", "Goodyear", 80000)
-        emp_2 = employee.Employee("Midas", "Goodyear", 90000)
-        self.assertEqual(emp_1.email, "Hachi.Goodyear@email.com")
-        self.assertEqual(emp_2.email, "Midas.Goodyear@email.com")
+        self.assertEqual(self.emp_1.email, "Hachi.Goodyear@email.com")
+        self.assertEqual(self.emp_2.email, "Midas.Goodyear@email.com")
 
-        emp_1.first = "Not-Muffin"
-        emp_2.last = "Daoust"
-        self.assertEqual(emp_1.email, "Not-Muffin.Goodyear@email.com")
-        self.assertEqual(emp_2.email, "Midas.Daoust@email.com")
+        self.emp_1.first = "Not-Muffin"
+        self.emp_2.last = "Daoust"
+        self.assertEqual(self.emp_1.email, "Not-Muffin.Goodyear@email.com")
+        self.assertEqual(self.emp_2.email, "Midas.Daoust@email.com")
 
     def test_fullname(self):
         """tests employee.fullname()"""
-        emp_1 = employee.Employee("Hachi", "Goodyear", 80000)
-        emp_2 = employee.Employee("Midas", "Goodyear", 90000)
-        self.assertEqual(emp_1.fullname, "Hachi Goodyear")
-        self.assertEqual(emp_2.fullname, "Midas Goodyear")
+        self.assertEqual(self.emp_1.fullname, "Hachi Goodyear")
+        self.assertEqual(self.emp_2.fullname, "Midas Goodyear")
 
-        emp_1.first = "Not-Muffin"
-        emp_2.last = "Daoust"
-        self.assertEqual(emp_1.fullname, "Not-Muffin Goodyear")
-        self.assertEqual(emp_2.fullname, "Midas Daoust")
+        self.emp_1.first = "Not-Muffin"
+        self.emp_2.last = "Daoust"
+        self.assertEqual(self.emp_1.fullname, "Not-Muffin Goodyear")
+        self.assertEqual(self.emp_2.fullname, "Midas Daoust")
 
     def test_apply_raise(self):
         """tests employee.apply_raise()"""
-        emp_1 = employee.Employee("Hachi", "Goodyear", 80000)
-        emp_2 = employee.Employee("Midas", "Goodyear", 90000)
+        self.emp_1.apply_raise()
+        self.emp_2.apply_raise()
+        self.assertEqual(self.emp_1.pay, 84000)
+        self.assertEqual(self.emp_2.pay, 94500)
 
-        emp_1.apply_raise()
-        emp_2.apply_raise()
-        self.assertEqual(emp_1.pay, 84000)
-        self.assertEqual(emp_2.pay, 94500)
-
-        emp_1.apply_raise()
-        self.assertEqual(emp_1.pay, 88200)
+        self.emp_1.apply_raise()
+        self.assertEqual(self.emp_1.pay, 88200)
 
     # def test_monthly_schedule(self):
     #     pass
